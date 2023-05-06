7b476f7d930deb273e9d65be429f688e708952cb
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-05-06 11:23:30 +0900

    Support relayer/v2

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
+	"context"
 	"embed"
 	"encoding/json"
-	"io/fs"
 	"log"
-	"net/http"
 	"os"
 
-	"github.com/fiatjaf/relayer"
-	"github.com/fiatjaf/relayer/storage/sqlite3"
+	"github.com/fiatjaf/relayer/v2"
+	"github.com/fiatjaf/relayer/v2/storage/sqlite3"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/nbd-wtf/go-nostr"
 	"github.com/nbd-wtf/go-nostr/nip11"
@@ -34,31 +33,41 @@ func (r *Relay) Name() string {
 	return "nostr-relay"
 }
 
-func (r *Relay) Storage() relayer.Storage {
+func (r *Relay) Storage(ctx context.Context) relayer.Storage {
 	return r.storage
 }
 
 func (r *Relay) OnInitialized(s *relayer.Server) {
-	sub, _ := fs.Sub(assets, "static")
-	s.Router().PathPrefix("/").Handler(http.FileServer(http.FS(sub)))
+	//sub, _ := fs.Sub(assets, "static")
+	//s.Router().PathPrefix("/").Handler(http.FileServer(http.FS(sub)))
 }
 
-func (r *Relay) Init() error                       { return nil }
-func (r *Relay) AcceptEvent(evt *nostr.Event) bool { return true }
-func (r *Relay) BeforeSave(evt *nostr.Event)       {}
+func (r *Relay) Init() error {
+	return nil
+}
+
+func (r *Relay) AcceptEvent(ctx context.Context, evt *nostr.Event) bool {
+	return true
+}
+
+func (r *Relay) BeforeSave(evt *nostr.Event) {
+}
+
 func (r *Relay) AfterSave(evt *nostr.Event) {
 	json.NewEncoder(os.Stderr).Encode(evt)
 }
+
 func (r *Relay) ServiceURL() string {
 	return "https://nostr.compile-error.net"
 }
+
 func (r *Relay) GetNIP11InformationDocument() nip11.RelayInformationDocument {
 	return nip11.RelayInformationDocument{
 		Name:          "nostr-relay",
 		Description:   "relay powered by the relayer framework",
 		PubKey:        "npub1937vv2nf06360qn9y8el6d8sevnndy7tuh5nzre4gj05xc32tnwqauhaj6",
 		Contact:       "mattn.jp@gmail.com",
-		SupportedNIPs: []int{9, 11, 12, 15, 16, 20, 49},
+		SupportedNIPs: []int{9, 11, 12, 15, 16, 20, 33, 42, 49},
 		Software:      "https://github.com/mattn/nostr-relay",
 		Version:       version,
 	}
@@ -68,10 +77,13 @@ func main() {
 	r := Relay{}
 	if err := envconfig.Process("", &r); err != nil {
 		log.Fatalf("failed to read from env: %v", err)
-		return
 	}
 	r.storage = &sqlite3.SQLite3Backend{DatabaseURL: os.Getenv("DATABASE_URL")}
-	if err := relayer.Start(&r); err != nil {
+	server, err := relayer.NewServer(&r)
+	if err != nil {
+		log.Fatalf("failed to create server: %v", err)
+	}
+	if err := server.Start("0.0.0.0", 7447); err != nil {
 		log.Fatalf("server terminated: %v", err)
 	}
 }
