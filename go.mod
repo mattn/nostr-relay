5fda05df336b19a389b6cc35304c11154dff4409
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-05-11 10:58:39 +0900

    version

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/mattn/nostr-relay
 go 1.18
 
 require (
-	github.com/fiatjaf/relayer/v2 v2.0.0-20230503201342-54001282d96b
+	github.com/fiatjaf/relayer/v2 v2.0.0-20230508154911-c4a678da1ec9
 	github.com/kelseyhightower/envconfig v1.4.0
-	github.com/nbd-wtf/go-nostr v0.17.3
+	github.com/nbd-wtf/go-nostr v0.18.1
 )
 
 require (
@@ -14,12 +14,18 @@ require (
 	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.2 // indirect
 	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
-	github.com/fiatjaf/relayer v1.7.3 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.2.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/jmoiron/sqlx v1.3.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/rs/cors v1.9.0 // indirect
-	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	golang.org/x/exp v0.0.0-20230510235704-dd950f8aeaea // indirect
+	golang.org/x/sys v0.8.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"embed"
 	"encoding/json"
+	"io/fs"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/fiatjaf/relayer/v2"
@@ -35,11 +37,6 @@ func (r *Relay) Name() string {
 
 func (r *Relay) Storage(ctx context.Context) relayer.Storage {
 	return r.storage
-}
-
-func (r *Relay) OnInitialized(s *relayer.Server) {
-	//sub, _ := fs.Sub(assets, "static")
-	//s.Router().PathPrefix("/").Handler(http.FileServer(http.FS(sub)))
 }
 
 func (r *Relay) Init() error {
@@ -73,6 +70,11 @@ func (r *Relay) GetNIP11InformationDocument() nip11.RelayInformationDocument {
 	}
 }
 
+type Info struct {
+	Version string `json:"version"`
+	Count   int64  `json:"count"`
+}
+
 func main() {
 	r := Relay{}
 	if err := envconfig.Process("", &r); err != nil {
@@ -83,6 +85,18 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to create server: %v", err)
 	}
+	sub, _ := fs.Sub(assets, "static")
+	server.Router().HandleFunc("/info", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("content-type", "application/json")
+		info := Info{
+			Version: version,
+		}
+		if err := r.storage.QueryRow("select count(*) from event").Scan(&info.Count); err != nil {
+			log.Println(err)
+		}
+		json.NewEncoder(w).Encode(info)
+	})
+	server.Router().Handle("/", http.FileServer(http.FS(sub)))
 	if err := server.Start("0.0.0.0", 7447); err != nil {
 		log.Fatalf("server terminated: %v", err)
 	}
