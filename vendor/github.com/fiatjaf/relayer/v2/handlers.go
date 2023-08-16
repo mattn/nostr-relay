0fc34e96b6bb69da52a77f58a958894869a6bbe8
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-08-16 21:02:24 +0900

    break ping look

--- vendor/github.com/fiatjaf/relayer/v2/handlers.go
+++ vendor/github.com/fiatjaf/relayer/v2/handlers.go
@@ -386,7 +386,10 @@ func (s *Server) HandleWebsocket(w http.ResponseWriter, r *http.Request) {
 
 		for {
 			select {
-			case <-ticker.C:
+			case _, ok := <-ticker.C:
+				if !ok {
+					return
+				}
 				err := ws.WriteMessage(websocket.PingMessage, nil)
 				if err != nil {
 					s.Log.Errorf("error writing ping: %v; closing websocket", err)
