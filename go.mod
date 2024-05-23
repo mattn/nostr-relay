95aa998fdabf22ccfedc15e413f4c8f791d1031b
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-05-24 00:50:11 +0900

    opensearch

--- go.mod
+++ go.mod
@@ -3,54 +3,49 @@ module github.com/mattn/nostr-relay
 go 1.22.0
 
 require (
-	github.com/fiatjaf/eventstore v0.3.13
+	github.com/fiatjaf/eventstore v0.4.2
 	github.com/fiatjaf/relayer/v2 v2.1.12
-	github.com/jmoiron/sqlx v1.3.5
+	github.com/jmoiron/sqlx v1.4.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/mattn/go-sqlite3 v1.14.22
-	github.com/nbd-wtf/go-nostr v0.29.3
+	github.com/nbd-wtf/go-nostr v0.31.2
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/aquasecurity/esquery v0.2.0 // indirect
-	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.3 // indirect
 	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
 	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
-	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
-	github.com/elastic/elastic-transport-go/v8 v8.4.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
 	github.com/elastic/go-elasticsearch/v7 v7.17.10 // indirect
-	github.com/elastic/go-elasticsearch/v8 v8.12.0 // indirect
 	github.com/fasthttp/websocket v1.5.8 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
-	github.com/go-logr/logr v1.4.1 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/go-sql-driver/mysql v1.8.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
-	github.com/gobwas/ws v1.3.2 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/klauspost/compress v1.17.7 // indirect
+	github.com/klauspost/compress v1.17.8 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/opensearch-project/opensearch-go/v4 v4.0.0 // indirect
 	github.com/puzpuzpuz/xsync/v3 v3.1.0 // indirect
-	github.com/rs/cors v1.10.1 // indirect
+	github.com/rs/cors v1.11.0 // indirect
 	github.com/savsgio/gotils v0.0.0-20240303185622-093b76447511 // indirect
 	github.com/tidwall/gjson v1.17.1 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.52.0 // indirect
-	go.opentelemetry.io/otel v1.23.1 // indirect
-	go.opentelemetry.io/otel/metric v1.23.1 // indirect
-	go.opentelemetry.io/otel/trace v1.23.1 // indirect
-	golang.org/x/exp v0.0.0-20240318143956-a85f2c67cd81 // indirect
-	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	github.com/valyala/fasthttp v1.53.0 // indirect
+	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 )
 
 //replace github.com/fiatjaf/relayer/v2 => ../../go/src/github.com/fiatjaf/relayer
 
 //replace github.com/fiatjaf/eventstore => ../../go/src/github.com/fiatjaf/eventstore
+replace github.com/fiatjaf/eventstore => ../../go/src/github.com/fiatjaf/eventstore
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"io/fs"
 	"log"
 	"log/slog"
+	"net"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
@@ -16,8 +17,8 @@ import (
 	"time"
 
 	"github.com/fiatjaf/eventstore"
-	"github.com/fiatjaf/eventstore/elasticsearch"
 	"github.com/fiatjaf/eventstore/mysql"
+	"github.com/fiatjaf/eventstore/opensearch"
 	"github.com/fiatjaf/eventstore/postgresql"
 	"github.com/fiatjaf/eventstore/sqlite3"
 	"github.com/fiatjaf/relayer/v2"
@@ -44,11 +45,11 @@ var (
 )
 
 type Relay struct {
-	driverName           string
-	sqlite3Storage       *sqlite3.SQLite3Backend
-	postgresStorage      *postgresql.PostgresBackend
-	mysqlStorage         *mysql.MySQLBackend
-	elasticsearchStorage *elasticsearch.ElasticsearchStorage
+	driverName        string
+	sqlite3Storage    *sqlite3.SQLite3Backend
+	postgresStorage   *postgresql.PostgresBackend
+	mysqlStorage      *mysql.MySQLBackend
+	opensearchStorage *opensearch.OpensearchStorage
 
 	mu        sync.Mutex
 	allowlist []string
@@ -67,7 +68,7 @@ func (r *Relay) DB() *sqlx.DB {
 		return r.postgresStorage.DB
 	case "mysql":
 		return r.mysqlStorage.DB
-	case "elasticsearch":
+	case "opensearch":
 		return nil
 	default:
 		panic("unsupported backend driver")
@@ -82,8 +83,8 @@ func (r *Relay) Storage(ctx context.Context) eventstore.Store {
 		return r.postgresStorage
 	case "mysql":
 		return r.mysqlStorage
-	case "elasticsearch":
-		return r.elasticsearchStorage
+	case "opensearch":
+		return r.opensearchStorage
 	default:
 		panic("unsupported backend driver")
 	}
@@ -277,9 +278,11 @@ func init() {
 func main() {
 	var r Relay
 	var ver bool
+	var addr string
 	var databaseURL string
 
-	flag.StringVar(&r.driverName, "driver", "sqlite3", "driver name (sqlite3/postgresql/mysql)")
+	flag.StringVar(&addr, "addr", "0.0.0.0:7447", "listen address")
+	flag.StringVar(&r.driverName, "driver", "sqlite3", "driver name (sqlite3/postgresql/mysql/opensearch)")
 	flag.StringVar(&databaseURL, "database", envDef("DATABASE_URL", "nostr-relay.sqlite"), "connection string")
 	flag.BoolVar(&ver, "version", false, "show version")
 	flag.Parse()
@@ -287,6 +290,15 @@ func main() {
 	if ver {
 		fmt.Println(version)
 		os.Exit(0)
+	}
+
+	host, sport, err := net.SplitHostPort(addr)
+	if err != nil {
+		log.Fatalf("failed to parse address: %v", err)
+	}
+	port, err := net.LookupPort("tcp", sport)
+	if err != nil {
+		log.Fatalf("failed to parse port number: %v", err)
 	}
 
 	go func() {
@@ -313,10 +325,11 @@ func main() {
 			QueryLimit:     relayLimitationDocument.MaxLimit,
 			QueryTagsLimit: relayLimitationDocument.MaxEventTags,
 		}
-	case "elasticsearch":
-		r.elasticsearchStorage = &elasticsearch.ElasticsearchStorage{
+	case "opensearch":
+		r.opensearchStorage = &opensearch.OpensearchStorage{
 			URL:       databaseURL,
 			IndexName: "",
+			Insecure:  true,
 		}
 	default:
 		fmt.Fprintln(os.Stderr, "unsupported backend driver:", r.driverName)
@@ -354,7 +367,7 @@ func main() {
 	server.Router().Handle("/", http.FileServer(http.FS(sub)))
 
 	server.Log = &r
-	if err := server.Start("0.0.0.0", 7447); err != nil {
+	if err := server.Start(host, port); err != nil {
 		log.Fatalf("server terminated: %v", err)
 	}
 }
