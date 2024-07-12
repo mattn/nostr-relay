0313a2c157bfba5a3696561fd875fb6c48db7e69
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-07-13 00:47:32 +0900

    fix supported nips

--- main.go
+++ main.go
@@ -147,7 +147,7 @@ func (r *Relay) GetNIP11InformationDocument() nip11.RelayInformationDocument {
 		Description:    "relay powered by the relayer framework",
 		PubKey:         "2c7cc62a697ea3a7826521f3fd34f0cb273693cbe5e9310f35449f43622a5cdc",
 		Contact:        "mattn.jp@gmail.com",
-		SupportedNIPs:  []int{1, 2, 4, 9, 11, 12, 15, 16, 20, 22, 33, 42, 45, 50},
+		SupportedNIPs:  []int{1, 2, 4, 9, 11, 12, 15, 16, 20, 22, 28, 33, 42, 45, 50},
 		Software:       "https://github.com/mattn/nostr-relay",
 		Icon:           "https://mattn.github.io/assets/image/mattn-mohawk.webp",
 		Version:        version,
